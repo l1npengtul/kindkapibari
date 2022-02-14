65514f98846a90963da8998055bbcf13a11ca3d3
l1npengtul <l1npengtul@protonmail.com>
2022-02-14 10:52:44 +0900

    refactor types out of compiler annd into a shared core crate

---
+++ affirmpak/affirmpak_core/src/lib.rs
@@ -0,0 +1,9 @@
+mod compiler;
+mod error;
+mod libjson;
+mod manifest;
+mod output;
+mod text;
+
+#[macro_use]
+extern crate serde;
--- affirmpak/compiler/src/main.rs
+++ affirmpak/compiler/src/main.rs
@@ -1,13 +1,3 @@
-mod compiler;
-mod libjson;
-mod manifest;
-mod text;
-mod error;
-mod output;
-
-#[macro_use]
-extern crate serde;
-
 fn main() {
     println!("Hello, world!");
 }
