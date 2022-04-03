ae86159a1d106b410774d82abfd270a7521ba789
l1npengtul <l1npengtul@protonmail.com>
2022-04-04 03:51:20 +0900

    move kindkapybari to kindkapibari

--- coconutpak/coconutpak_island/src/main.rs
+++ coconutpak/coconutpak_island/src/main.rs
@@ -4,10 +4,12 @@
 
 use crate::config::Config;
 use argon2::{Algorithm, Argon2, Params, Version};
+use kindkapibari_core::reseedingrng::AutoReseedingRng;
 use once_cell::sync::Lazy;
 use redis::aio::ConnectionManager;
 use redis::Client;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 static CONFIG: Lazy<Arc<Config>> = Lazy::new(|| {
     let config = Config::load().unwrap_or_default();
@@ -22,8 +24,8 @@ mod api;
 mod coconutpak_cleanup;
 mod coconutpak_compiler;
 mod config;
-mod schema;
 mod login;
+mod schema;
 
 #[tokio::main]
 async fn main() {
