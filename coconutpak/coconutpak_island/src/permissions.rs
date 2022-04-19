7d403cbd9c1f0d4f9a451bd3c98bc52c21f471a8
l1npengtul <l1npengtul@protonmail.com>
2022-04-20 07:49:12 +0900

    permission?

--- coconutpak/coconutpak_island/src/permissions.rs
+++ coconutpak/coconutpak_island/src/permissions.rs
@@ -1,12 +1,4 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
-pub enum Permissions {
-    Server,
-    Admin,
-    Demote,
-    Promote,
-    YankSelf,
-    Publish,
-    Download,
-}
+pub enum Permissions {}
