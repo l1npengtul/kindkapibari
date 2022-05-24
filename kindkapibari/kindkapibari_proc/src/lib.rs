79232de650c915376803ccfd0274b2c684f15e28
l1npengtul <l1npengtul@protonmail.com>
2022-05-24 15:08:33 +0900

    get most of enum string procmacro done

--- kindkapibari/kindkapibari_proc/src/lib.rs
+++ kindkapibari/kindkapibari_proc/src/lib.rs
@@ -1,26 +1,119 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DataEnum, DataUnion, DeriveInput, FieldsNamed, FieldsUnnamed};
 use quote::quote;
+use syn::{parse_macro_input, DataEnum, DeriveInput};
 
 #[proc_macro_derive(AttrString)]
-pub fn AttrString(input: TokenStream) -> TokenStream {
-    let DeriveInput {ident, data, ..} = parse_macro_input!(input);
+pub fn attr_string(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
 
     let mut fields = vec![];
-    if let syn::Data::Enum(DataEnum {variants, ..}) = data {
-        variants.into_iter().for_each(|variant| {
-            let festr = variant.fields.into_iter().map(|enum_f| {
-                
-            })
-        })
+    let mut field = vec![];
 
+    let mut parse_noarg = vec![];
+    let mut parse_onearg = vec![];
+    let mut parse_plain_string_from_noarg = vec![];
+    let mut parse_plain_string_from_onearg = vec![];
+    if let syn::Data::Enum(DataEnum { variants, .. }) = data {
+        variants.into_iter().for_each(|variant| {
+            // Enum -> String
+            match variant.fields {
+                syn::Fields::Named(n) => {
+                    let flds = n.named.into_iter().next().unwrap();
+
+                    parse_onearg.push(quote! {
+                        {
+                            <variant>{flds.ident.unwrap(): subtag.parse()?}
+                        }
+                    });
+                    parse_plain_string_from_onearg.push(variant.ident.to_string());
+                    fields.push(quote! {
+                        {
+                            let mut base_str = <variant.ident>::to_string();
+                            format!("{base_str}({})", flds.to_string())
+                        }
+                    });
+                }
+                syn::Fields::Unnamed(u) => {
+                    let flds = u.unnamed.into_iter().next().unwrap();
+
+                    parse_onearg.push(quote! {
+                        {
+                            <variant>(subtag.parse()?)
+                        }
+                    });
+                    parse_plain_string_from_onearg.push(variant.ident.to_string());
+
+                    fields.push(quote! {
+                        {
+                            let mut base_str = <variant.ident>::to_string();
+                            format!("{base_str}({})", flds.to_string())
+                        }
+                    });
+                }
+                syn::Fields::Unit => {
+                    parse_noarg.push(quote! {
+                        {
+                            variant
+                        }
+                    });
+                    parse_plain_string_from_noarg.push(variant.ident.to_string());
+                    fields.push(quote! {
+                        {
+                            <variant.ident>::to_string()
+                        }
+                    });
+                }
+            }
+            field.push(variant.ident);
+        })
     }
 
     let out = quote! {
+        impl Debug for #ident {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                write!(
+                    f,
+                    "{}",
+                    self.to_attr_string()
+                )
+            }
+        }
+
+        impl Display for #ident {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{self:?}")
+            }
+        }
+
+        impl FromStr for #ident {
+            type Err = Self::ParseError;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let s.replace([ALLOWED_CHARS, &['(', ')']].concat(), "") != "" {
+                    return Err(Self::Err::from("not allowed characters".to_string()));
+                }
+
+                let splitted = s.split(&['(', ')']).collect::<Vec<String>>();
+                if splitted.len() == 1 {
+                    Ok(match splitted[0].as_str() {
+                        #(#parse_plain_string_from_noarg => #parse_noarg),*
+                    })
+                } else splitted.len() == 2 {
+                    let subtag = splitted[1];
+
+                    Ok(match splitted[0].as_str() {
+                        #(#parse_plain_string_from_onearg => #parse_onearg),*
+                    })
+                } else {
+                    return Err(Self::Err::from("not found".to_string()));
+                }
+            }
+        }
+
         impl #ident {
             pub fn to_attr_string(&self) -> String {
                 match &self {
-                    #(#fields => <#fields>::to_string()),*
+                    #(#field => #fields),*
                 }
             }
 
