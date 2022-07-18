5ac8bd658a9c5f8aaa53fc042a4dc469cd95739b
l1npengtul <l1npengtul@protonmail.com>
2022-07-18 13:38:21 +0900

    add actions

--- kindkapibari/game/kindkapibari_client/src/keys.rs
+++ kindkapibari/game/kindkapibari_client/src/keys.rs
@@ -1,4 +1,32 @@
+use bevy::prelude::KeyCode;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Actions {
-    PrimaryAttack,
-    SecondaryAttack,
+    AttackPrimary,
+    AttackSecondary,
+    AttackActivate,
+    AttackThird,
+    AttackThirdEmulateKey,
+    MoveForward,
+    MoveLeft,
+    MoveBack,
+    MoveRight,
+    Reload,
+    Interact,
+    Inventory,
+    WeaponPrimary,
+    WeaponSecondary,
+    WeaponThird,
+    Options,
+    ChatGlobal,
+    ChatGuild,
+    ChatParty,
+}
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct InputMap {
+    imap: HashMap<KeyCode, Actions>,
 }
